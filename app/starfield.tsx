b7295746e6a56bc721168f025443cc6b7df8384f
Aditya Saravana <aditya.saravana@icloud.com>
2026-03-13 20:01:59 -0700

    forgot to stage starfield

---
+++ app/starfield.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { StarTrip } from "@helperd/starfield-react";
+
+export default function Starfield() {
+  return <StarTrip speed={0} backgroundColor="transparent" zIndex={-1} />;
+}
